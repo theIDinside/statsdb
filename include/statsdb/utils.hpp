0e755243134b7cac57d6212db6dc8f15eb1b98ee
Simon Farre <simon.farre.cx@gmail.com>
2021-02-14 14:15:32 +0100

    Added the Optional data type, that probably will be used later on. If not, it will be removed.

--- include/statsdb/utils.hpp
+++ include/statsdb/utils.hpp
@@ -3,17 +3,44 @@
 //
 
 #pragma once
-#include <vector>
-#include <numeric>
 #include <cassert>
+#include <numeric>
 #include <ranges>
 #include <span>
+#include <vector>
 
 
 namespace utils {
+    template<typename T>
+    struct Optional {
+        std::optional<T> value{};
+
+        template<typename Fn>
+        using MapResult = Optional<typename std::invoke_result<Fn, T>::type>;
+
+        template<typename Fn>
+        auto and_then(Fn fn) -> MapResult<Fn> {
+            if (value)
+                return {fn(value.value())};
+            else
+                return {};
+        }
+
+        auto unwrap_or(T&& t) {
+            if (value)
+                return value.value();
+            else
+                return t;
+        }
+
+        constexpr bool has_value() const { return value.has_value(); }
+        constexpr T& get_value() { return value.value(); }
+    };
+
     using WSize = std::size_t;
 
-    template <typename Container> concept IterableContainer = requires(Container c) {
+    template<typename Container>
+    concept IterableContainer = requires(Container c) {
         c.begin();
         c.cbegin();
         c.end();
@@ -22,53 +49,56 @@ namespace utils {
         c.size();
     };
 
-    template <typename Number> concept FloatingPoint = requires(Number num) {
-            std::is_floating_point_v<Number>;
+    template<typename Number>
+    concept FloatingPoint = requires(Number num) {
+        std::is_floating_point_v<Number>;
     };
 
     /**
      * Helper function. To keep myself from the insanity of typing out the static_cast calls everywhere.
      */
-    template <typename TValue, typename SpanValue>
+    template<typename TValue, typename SpanValue>
     inline double avg(TValue val, SpanValue sp) {
         return static_cast<double>(val) / static_cast<double>(sp);
     }
 
     template<IterableContainer C, typename OutputIt, typename RangeFn, typename T = typename OutputIt::container_type::value_type>
-    void container_window_acc(const C& c, std::size_t window_size, OutputIt out_iterator, RangeFn fn) {
+    void container_window_acc(const C &c, std::size_t window_size, OutputIt out_iterator, RangeFn fn) {
         assert(window_size <= c.size());
         auto begin = c.cbegin();
         auto container_end = c.cend();
         auto window_end = c.cbegin() + window_size;
-        for(; window_end <= container_end; window_end++, begin++) {
+        for (; window_end <= container_end; window_end++, begin++) {
             auto v = std::accumulate(begin, window_end, T{}, fn);
             *out_iterator = std::move(v);
         }
     }
 
-    template <typename AccFn, typename ElType> concept AverageFn = requires(AccFn f) {
+    template<typename AccFn, typename ElType>
+    concept AverageFn = requires(AccFn f) {
         f(float{}, ElType{});
     };
 
-    template<class ElementType, std::size_t SpanWidth> [[nodiscard]]
-    constexpr auto slide(std::span<ElementType, SpanWidth> span, std::size_t offset, std::size_t width) {
+    template<class ElementType, std::size_t SpanWidth>
+    [[nodiscard]] constexpr auto slide(std::span<ElementType, SpanWidth> span, std::size_t offset, std::size_t width) {
         return span.subspan(offset, offset + width <= span.size() ? width : 0U);
     }
 
-    template<typename T, std::size_t N, typename AccFn>
-    auto span_accumulate(std::span<T, N> span, AccFn fn) {
-        return std::accumulate(span.begin(), span.end(), T{}, fn);
+    template<typename C, typename AccFn>
+    auto accumulate(C c, AccFn fn) {
+        using T = typename C::reference;
+        return std::accumulate(c.begin(), c.end(), T{}, fn);
     }
     /// Calculates the rolling span average, of a span of window_size, across the elements in c
-    template <typename Number, IterableContainer C, typename AccFn>
-    auto window_average(const C& container, std::size_t window_size, AccFn fn) {
+    template<typename Number, IterableContainer C, typename AccFn>
+    auto window_average(const C &container, std::size_t window_size, AccFn fn) {
         assert(window_size <= container.size());
         std::vector<Number> results;
         std::span season_span{container};
-        for(std::size_t offset = 0; ; ++offset) {
+        for (std::size_t offset = 0;; ++offset) {
             auto game_span = slide(season_span, offset, window_size);
-            if(game_span.empty()) break;
-            float value = span_accumulate(game_span, fn);
+            if (game_span.empty()) break;
+            float value = accumulate(game_span, fn);
             Number res = float(value / window_size);
             results.push_back(res);
         }
@@ -86,7 +116,7 @@ namespace utils {
     void accumulate_windows(Iterator begin, Iterator end, std::size_t window_size, OutputIterator out_iterator, AccumulatorFn acc_fn) {
         assert(static_cast<unsigned int>(window_size) <= (end - begin));
         auto window_end = begin + window_size;
-        for(; window_end <= end; window_end++, begin++) {
+        for (; window_end <= end; window_end++, begin++) {
             auto v = std::accumulate(begin, window_end, T{}, acc_fn);
             *out_iterator = std::move(v);
         }
@@ -96,11 +126,11 @@ namespace utils {
     void accumulate_window_transform(Iterator begin, Iterator end, WSize window_size, OutputIt out_iterator, AccumulatorFn r_fn, MapFn map_to) {
         assert(static_cast<unsigned int>(window_size) <= (end - begin));
         auto window_end = begin + window_size;
-        for(; window_end <= end; window_end++, begin++) {
+        for (; window_end <= end; window_end++, begin++) {
             auto v = std::accumulate(begin, window_end, T{}, r_fn);
             auto tv = map_to(v);
             *out_iterator = std::move(tv);
         }
     }
 
-}
+}// namespace utils
