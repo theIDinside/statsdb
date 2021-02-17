993eb7b82740868041c38662c73c1b1f5878fb21
Simone Farre <simon.farre.cx@gmail.com>
2021-02-17 18:10:08 +0100

    added operator+ and operator- for RoundedDecimalNumber class

--- include/statsdb/decimal_number.hpp
+++ include/statsdb/decimal_number.hpp
@@ -15,19 +15,78 @@ constexpr auto pow_10() {
 
 /// Utility class. Templated on the amount of decimal points / what accuracy the number is. can implicitly be converted to/from floats
 /// So DecimalNumber<2> num = 0.547f -> 0.55 and DecimalNumber<1> num = 0.547 -> 0.5
+/// This class is basically used when we want to display numbers. If you want to do math with them, be-fucking-wary
 template<int DecimalPoints>
 struct RoundedDecimalNumber {
+    static_assert(DecimalPoints > 0, "Template parameter DecimalPoints error: Decimal 'precision' must be > 0");
     constexpr RoundedDecimalNumber() : value_{0.0f} {}
-    constexpr RoundedDecimalNumber(float f) {
-        float pow = pow_10<DecimalPoints>();
-        float tmp = std::round(f * pow);
-        value_ = float(tmp) / pow;
+    constexpr RoundedDecimalNumber(float f) : value_(f) {
+
     }
-    constexpr operator float() const { return value_; }
-    constexpr float value() const { return value_; }
+    constexpr operator float() const {
+        float pow = pow_10<DecimalPoints>();
+        float tmp = std::round(value_ * pow);
+        return float(tmp) / pow;
+    }
+    constexpr float value() const {
+        float pow = pow_10<DecimalPoints>();
+        float tmp = std::round(value_ * pow);
+        return float(tmp) / pow;
+    }
+    static constexpr auto precision() { return PRECISION; }
 private:
     float value_;
+    static constexpr int PRECISION = DecimalPoints;
+
+
+    template <int DPL, int DPR>
+    friend constexpr auto operator+(RoundedDecimalNumber<DPL>, RoundedDecimalNumber<DPR>);
+    template<int DPL, int DPR>
+    friend constexpr auto operator-(RoundedDecimalNumber<DPL>, RoundedDecimalNumber<DPR>);
+    template <int DP>
+    friend constexpr auto operator+(RoundedDecimalNumber<DP>, float);
+    template<int DP>
+    friend constexpr auto operator-(RoundedDecimalNumber<DP>, float);
+    template <int DP>
+    friend constexpr auto operator+(float, RoundedDecimalNumber<DP>);
+    template<int DP>
+    friend constexpr auto operator-(float, RoundedDecimalNumber<DP>);
 };
+
+template <int DPL, int DPR>
+constexpr auto operator+(RoundedDecimalNumber<DPL> lhs, RoundedDecimalNumber<DPR> rhs) {
+    if constexpr(RoundedDecimalNumber<DPL>::PRECISION < RoundedDecimalNumber<DPR>::PRECISION) {
+        return RoundedDecimalNumber<DPR>{lhs.value_ + rhs.value_};
+    } else {
+        return RoundedDecimalNumber<DPL>{lhs.value_ + rhs.value_};
+    }
+}
+
+template <int DPL, int DPR>
+constexpr auto operator-(RoundedDecimalNumber<DPL> lhs, RoundedDecimalNumber<DPR> rhs) {
+    if constexpr(RoundedDecimalNumber<DPL>::PRECISION < RoundedDecimalNumber<DPR>::PRECISION) {
+        return RoundedDecimalNumber<DPR>{lhs.value_ + rhs.value_};
+    } else {
+        return RoundedDecimalNumber<DPL>{lhs.value_ + rhs.value_};
+    }
+}
+
+template <int DP>
+constexpr auto operator+(RoundedDecimalNumber<DP> lhs, float rhs) {
+    return RoundedDecimalNumber<DP>{lhs.value_ + rhs};
+}
+template<int DP>
+constexpr auto operator-(RoundedDecimalNumber<DP> lhs, float rhs) {
+    return RoundedDecimalNumber<DP>{lhs.value_ - rhs};
+}
+template <int DP>
+constexpr auto operator+(float lhs, RoundedDecimalNumber<DP> rhs) {
+    return RoundedDecimalNumber<DP>{lhs + rhs.value_};
+}
+template<int DP>
+constexpr auto operator-(float lhs, RoundedDecimalNumber<DP> rhs) {
+    return RoundedDecimalNumber<DP>{lhs - rhs.value_};
+}
 
 using Number = RoundedDecimalNumber<2>;
 using RollingStandard = std::vector<Number>;
--- include/statsdb/utils.hpp
+++ include/statsdb/utils.hpp
@@ -140,5 +140,5 @@ namespace utils {
         c.begin();
         c.end();
     };
-    
+
 }// namespace utils
